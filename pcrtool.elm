82ad9b2446d1eced1ccd94c1164c0e919f6073d5
Andrew Donshik <andrew@donshikfamily.com>
2018-04-26 11:54:52 -0400

    basic text boxes

--- pcrtool.elm
+++ pcrtool.elm
@@ -6,21 +6,30 @@ import Html.Events as Events
 
 
 type alias AppState =
+    { sequence : String
+    , forward: String
+    , reverse: String}
 
 
 initialState : AppState
 initialState =
-
+    {  sequence = ""
+     , forward = ""
+     , reverse = ""}
 
 type AppEvent
-    = 
+    = UpdateSeq String |
+      UpdateFor String |
+      UpdateRev String
 
 
 view : AppState -> Html.Html AppEvent
 view state =
     Html.div []
-        [ Html.h1 [("text-align", "center")] [ Html.text "PCRTool" ]
-        , (Html.input [ Events.onInput (\s -> EditTask index s), Attrs.value task ] [])
+        [ (Html.h1 [Attrs.style [("text-align", "center")]] [ Html.text "PCRTool" ])
+        , (Html.input [ Events.onInput (\s -> UpdateSeq s), Attrs.placeholder "target sequence"] [])
+        , (Html.input [ Events.onInput (\s -> UpdateFor s), Attrs.placeholder "forward sequence"] [])
+        , (Html.input [ Events.onInput (\s -> UpdateRev s), Attrs.placeholder "reverse sequence"] [])
         ]
 
 
