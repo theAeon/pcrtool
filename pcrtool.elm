2098b2c0a410638f92b85534404450839f7b9ab5
Andrew Donshik <andrew@donshikfamily.com>
2018-05-01 14:11:24 -0400

    5/1 uptodate

--- pcrtool.elm
+++ pcrtool.elm
@@ -8,29 +8,42 @@ import Html.Events as Events
 type alias AppState =
     { sequence : String
     , forward: String
-    , reverse: String}
+    , reverse: String
+    , aminoacid: String
+    , modsequence : String}
 
 
 initialState : AppState
 initialState =
     {  sequence = ""
      , forward = ""
-     , reverse = ""}
-
+     , reverse = ""
+     , aminoacid = ""
+     , ModSequence = ""}
 type AppEvent
     = UpdateSeq String |
       UpdateFor String |
-      UpdateRev String
+      UpdateRev String |
+      ReCalculate
 
 
 view : AppState -> Html.Html AppEvent
 view state =
     Html.div []
         [ (Html.h1 [Attrs.style [("text-align", "center")]] [ Html.text "PCRTool" ])
-        , (Html.textarea [ Events.onInput (\s -> UpdateSeq s), Attrs.placeholder "target sequence"] [])
-        , (Html.textarea [ Events.onInput (\s -> UpdateFor s), Attrs.placeholder "forward sequence"] [])
-        , (Html.textarea [ Events.onInput (\s -> UpdateRev s), Attrs.placeholder "reverse sequence"] [])
-        ]
+        , (Html.textarea [ Events.onInput (\s -> UpdateSeq s), Attrs.placeholder "target sequence", Attrs.style [("resize", "none")]] [])
+        , (Html.textarea [ Events.onInput (\s -> UpdateFor s), Attrs.placeholder "forward sequence", Attrs.style [("resize", "none")]] [])
+        , (Html.textarea [ Events.onInput (\s -> UpdateRev s), Attrs.placeholder "reverse sequence", Attrs.style [("resize", "none")]] [])
+        , Html.div [] [Html.p [] [], Html.p [] []]]
+
+
+update : AppEvent -> AppState -> AppState
+update e s =
+    case e of
+        UpdateSeq str -> {s | sequence = str}
+        UpdateFor str -> {s | forward = str}
+        UpdateRev str -> {s | reverse = str}
+        ReCalculate -> {s | modsequence = , aminoacid = }
 
 
 main =
